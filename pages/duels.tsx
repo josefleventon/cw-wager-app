ab999ad7ba61a93d54b110608a28e81aab47473e
Josef Leventon <josef.leventon@gmail.com>
2023-05-23 14:31:40 -0400

    Add sound to Spectate button

--- pages/duels.tsx
+++ pages/duels.tsx
@@ -1,65 +1,65 @@
-import type { NextPage } from 'next'
+import type { NextPage } from "next";
 
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 
-import useChain from 'hooks/useChain'
-import { useRouter } from 'next/router'
-import { Spinner } from 'components'
-import { humanize } from 'util/constants'
-import { Job } from 'types/agent'
+import useChain from "hooks/useChain";
+import { useRouter } from "next/router";
+import { Spinner } from "components";
+import { humanize } from "util/constants";
+import { Job } from "types/agent";
 
-import useSound from 'use-sound';
+import useSound from "use-sound";
+import { useMusic } from "contexts/music";
 
 const Duels: NextPage = () => {
-  const { disconnect } = useChain()
-  const router = useRouter()
+  const { disconnect } = useChain();
+  const router = useRouter();
 
-  const [jobs, setJobs] = useState<Job[]>()
+  const { play, stop } = useMusic();
 
-  const [revalidateCounter, setRevalidateCounter] = useState(0)
-  const [factor, setFactor] = useState<1 | -1>(1)
+  const [jobs, setJobs] = useState<Job[]>();
 
-  const [playClick] = useSound(
-    '/sounds/click.mp3',
-    { volume: 0.5 }
-  );
+  const [revalidateCounter, setRevalidateCounter] = useState(0);
+  const [factor, setFactor] = useState<1 | -1>(1);
+
+  const [playClick] = useSound("/sounds/click.mp3", { volume: 0.5 });
 
   useEffect(() => {
-    fetch(process.env.NEXT_PUBLIC_AGENT_API! + '/jobs')
+    fetch(process.env.NEXT_PUBLIC_AGENT_API! + "/jobs")
       .then((res) => res.json())
       .then((json: { jobs: Job[] }) => {
-        console.log(json)
-        if (json.jobs) setJobs(json.jobs)
-      })
-  }, [revalidateCounter])
+        console.log(json);
+        if (json.jobs) setJobs(json.jobs);
+      });
+  }, [revalidateCounter]);
 
   useEffect(() => {
     setInterval(() => {
-      setRevalidateCounter(revalidateCounter + 1 * factor)
+      setRevalidateCounter(revalidateCounter + 1 * factor);
       switch (factor) {
         case -1:
-          setFactor(1)
-          break
+          setFactor(1);
+          break;
         case 1:
-          setFactor(-1)
-          break
+          setFactor(-1);
+          break;
       }
-    }, 10_000)
-  }, [])
+    }, 10_000);
+  }, []);
 
   return jobs ? (
     <main
       id="main"
       className="flex items-center justify-center w-screen h-[85vh] md:overflow-hidden"
     >
-      <div className="hidden md:block absolute flex flex-col space-x-2 top-1 right-2 z-10">
+      <div className="absolute z-10 flex flex-col hidden space-x-2 md:block top-1 right-2">
         <button
           id="connect-wallet"
           className="inline-flex items-center justify-center px-6 pt-4 pb-1 text-black bg-white hover:bg-slate-300"
           onClick={() => {
-            disconnect()
-            router.push('/')
-            playClick()
+            disconnect();
+            router.push("/");
+            playClick();
           }}
         >
           Disconnect
@@ -78,12 +78,12 @@ const Duels: NextPage = () => {
             {jobs.map((job) => (
               <div
                 key={job.wagers[0].token.token_id}
-                className="grid grid-cols-4 grid-flow-row md:grid-cols-5 gap-4 pt-4 px-4"
+                className="grid grid-flow-row grid-cols-4 gap-4 px-4 pt-4 md:grid-cols-5"
                 style={{
-                  background: '#E3FFFF50',
+                  background: "#E3FFFF50",
                 }}
               >
-                <div className='col-span-2 md:col-span-1'>
+                <div className="col-span-2 md:col-span-1">
                   <p className="text-sm md:text-lg">
                     Wizard #{job.wagers[0].token.token_id}
                   </p>
@@ -91,16 +91,20 @@ const Duels: NextPage = () => {
                     Wizard #{job.wagers[1].token.token_id}
                   </p>
                 </div>
-                <div className='col-span-1 md:col-span-1'>
-                  <p className="text-sm md:text-lg uppercase">${job.wagers[0].currency}</p>
-                  <p className="text-sm md:text-lg uppercase">${job.wagers[1].currency}</p>
+                <div className="col-span-1 md:col-span-1">
+                  <p className="text-sm uppercase md:text-lg">
+                    ${job.wagers[0].currency}
+                  </p>
+                  <p className="text-sm uppercase md:text-lg">
+                    ${job.wagers[1].currency}
+                  </p>
                 </div>
-                <div className='col-span-2 md:col-span-1 order-last'>
+                <div className="order-last col-span-2 md:col-span-1">
                   <p className="text-sm md:text-lg">{humanize(job.amount)}</p>
                   <p className="text-sm md:text-lg">
                     {(
                       (new Date(
-                        parseInt(job?.expires_at || '0') / 1_000_000,
+                        parseInt(job?.expires_at || "0") / 1_000_000
                       ).getTime() -
                         new Date().getTime()) /
                       60000
@@ -108,43 +112,55 @@ const Duels: NextPage = () => {
                     m left
                   </p>
                 </div>
-                <div className='col-span-1 md:col-span-1'>
+                <div className="col-span-1 md:col-span-1">
                   <div>
                     {job.current_winner?.token_id ===
                       job.wagers[0].token.token_id && (
-                      <p className="text-sm md:text-lg text-green-500">Winning...</p>
+                      <p className="text-sm text-green-500 md:text-lg">
+                        Winning...
+                      </p>
                     )}
                     {job.current_winner?.token_id !==
                       job.wagers[0].token.token_id &&
                       job.current_winner && (
-                        <p className="text-sm md:text-lg text-red-500">Losing...</p>
+                        <p className="text-sm text-red-500 md:text-lg">
+                          Losing...
+                        </p>
                       )}
-                    {!job.current_winner && <p className="text-sm md:text-lg">Tied</p>}
+                    {!job.current_winner && (
+                      <p className="text-sm md:text-lg">Tied</p>
+                    )}
                   </div>
                   <div>
                     {job.current_winner?.token_id ===
                       job.wagers[1].token.token_id && (
-                      <p className="text-sm md:text-lg text-green-500">Winning...</p>
+                      <p className="text-sm text-green-500 md:text-lg">
+                        Winning...
+                      </p>
                     )}
                     {job.current_winner?.token_id !==
                       job.wagers[1].token.token_id &&
                       job.current_winner && (
-                        <p className="text-sm md:text-lg text-red-500">Losing...</p>
+                        <p className="text-sm text-red-500 md:text-lg">
+                          Losing...
+                        </p>
                       )}
-                    {!job.current_winner && <p className="text-sm md:stext-lg">Tied</p>}
+                    {!job.current_winner && (
+                      <p className="text-sm md:stext-lg">Tied</p>
+                    )}
                   </div>
                 </div>
                 <button
                   id="connect-wallet"
                   className="inline-flex items-center col-span-2 md:col-span-1 order-last justify-center px-4 pt-3 pb-0.5 mb-4 text-sm text-black bg-white hover:bg-slate-300"
-                  onClick={() => 
-                    {
-                      router.push(
-                        `/status?token_id=${job.wagers[0].token.token_id}`,
-                      )
-                      playClick()
-                    }
-                  }
+                  onClick={() => {
+                    router.push(
+                      `/status?token_id=${job.wagers[0].token.token_id}`
+                    );
+                    stop();
+                    play("battle");
+                    playClick();
+                  }}
                 >
                   Spectate
                 </button>
@@ -157,11 +173,11 @@ const Duels: NextPage = () => {
   ) : (
     <main
       id="main"
-      className="flex items-center justify-center absolute w-screen h-screen md:overflow-hidden bg-theme-blue z-10"
+      className="absolute z-10 flex items-center justify-center w-screen h-screen md:overflow-hidden bg-theme-blue"
     >
       <Spinner className="w-16 h-16 text-white" />
     </main>
-  )
-}
+  );
+};
 
-export default Duels
+export default Duels;
